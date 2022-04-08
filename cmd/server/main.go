fa6e66ca2bdc9687f685e3c55b6fa570a57c9cc9
UBC Student <tfoolery@students.cs.ubc.ca>
2022-04-07 23:30:21 -0700

    Revert "Server RPC impl"

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,17 +1,5 @@
 package main
 
-import (
-	"cs.ubc.ca/cpsc416/onionRPC/onionRPC/server"
-	"cs.ubc.ca/cpsc416/onionRPC/util"
-	"fmt"
-)
-
 func main() {
-	var config server.Config
-	util.ReadJSONConfig("config/server_config.json", &config)
-	_server := server.Server{Config: config}
 
-	err := _server.Start(_server.Config)
-	fmt.Println(err)
-	select {}
 }

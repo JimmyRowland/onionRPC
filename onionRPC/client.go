530728564d7f756cc1a933fffb3e8a111d6050fe
ShermanZhu1 <shermanzhu1@gmail.com>
2022-04-10 13:38:51 -0700

    Added Client Tracing

--- onionRPC/client.go
+++ onionRPC/client.go
@@ -19,9 +19,50 @@ import (
 	"cs.ubc.ca/cpsc416/onionRPC/onionRPC/guardNode"
 	"cs.ubc.ca/cpsc416/onionRPC/onionRPC/relayNode"
 	"cs.ubc.ca/cpsc416/onionRPC/onionRPC/role"
+	"github.com/DistributedClocks/tracing"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
+
+// Actions to be recorded by client:
+
+type ClientStart struct {
+	ClientId string
+}
+
+type RpcCall struct {
+	ClientId      string
+	ServiceMethod string
+}
+
+type RpcResRecvd struct {
+	ClientId      string
+	ServiceMethod string
+	Result        interface{}
+}
+
+type OnionReq struct {
+	ClientId string
+}
+
+type OnionResRecvd struct {
+	ClientId  string
+	ExitAddr  string
+	RelayAddr string
+	GuardAddr string
+}
+
+type SharedSecretReq struct {
+	ClientId string
+	NodeType string
+}
+
+type SharedSecretResRecvd struct {
+	ClientId string
+	NodeType string
+}
+
+// ***
 
 type ClientConfig struct {
 	ClientID          string
@@ -41,10 +82,17 @@ type Client struct {
 	Guard        OnionNode
 	Exit         OnionNode
 	Relay        OnionNode
+	Tracer       *tracing.Tracer
 }
 
 func (client *Client) Start(config ClientConfig) {
 	client.ClientConfig = config
+	client.Tracer = tracing.NewTracer(tracing.TracerConfig{
+		ServerAddress:  config.TracingServerAddr,
+		TracerIdentity: config.TracingIdentity,
+	})
+	trace := client.Tracer.CreateTrace()
+	trace.RecordAction(ClientStart{ClientId: client.ClientConfig.ClientID})
 	client.getNodes("", "", "")
 	err := client.getGuardSharedSecret()
 	if err != nil {
@@ -80,6 +128,8 @@ type OnionCircuitResponse struct {
 }
 
 func (client *Client) getNodes(oldExitAddr, oldRelayAddr, oldGuardAddr string) {
+	trace := client.Tracer.CreateTrace()
+	trace.RecordAction(OnionReq{ClientId: client.ClientConfig.ClientID})
 	// TODO: https://www.figma.com/file/kP9OXD9I8nZgCYLmx5RpY4/Untitled?node-id=34%3A44
 	coordAddr := client.ClientConfig.CoordAddr
 	conn, err := net.Dial("tcp", coordAddr)
@@ -111,9 +161,17 @@ func (client *Client) getNodes(oldExitAddr, oldRelayAddr, oldGuardAddr string) {
 	client.Exit = msg.Exit
 	client.Guard = msg.Guard
 	client.Relay = msg.Relay
+	trace.RecordAction(OnionResRecvd{
+		ClientId:  client.ClientConfig.ClientID,
+		ExitAddr:  msg.Exit.RpcAddress,
+		RelayAddr: msg.Relay.RpcAddress,
+		GuardAddr: msg.Guard.RpcAddress})
 }
 
 func (client *Client) getGuardSharedSecret() error {
+	trace := client.Tracer.CreateTrace()
+	trace.RecordAction(SharedSecretReq{ClientId: client.ClientConfig.ClientID, NodeType: "Guard"})
+
 	priva, puba := role.GetPrivateAndPublicKey()
 	pubaBytes, _ := x509.MarshalPKIXPublicKey(&puba)
 
@@ -137,6 +195,7 @@ func (client *Client) getGuardSharedSecret() error {
 		sharedSecret := sha256.Sum256(shared.Bytes())
 		client.Guard.sharedSecret, _ = aes.NewCipher(sharedSecret[:])
 		client.Guard.sessionId = hex.EncodeToString(response.PublicKey)
+		trace.RecordAction(SharedSecretResRecvd{ClientId: client.ClientConfig.ClientID, NodeType: "Guard"})
 	default:
 		fmt.Println(string(response.PublicKey))
 		return errors.New("Unknown public key type")
@@ -145,6 +204,9 @@ func (client *Client) getGuardSharedSecret() error {
 }
 
 func (client *Client) getRelaySharedSecret() error {
+	trace := client.Tracer.CreateTrace()
+	trace.RecordAction(SharedSecretReq{ClientId: client.ClientConfig.ClientID, NodeType: "Relay"})
+
 	priva, puba := role.GetPrivateAndPublicKey()
 	pubaBytes, _ := x509.MarshalPKIXPublicKey(&puba)
 
@@ -168,6 +230,7 @@ func (client *Client) getRelaySharedSecret() error {
 		sharedSecret := sha256.Sum256(shared.Bytes())
 		client.Relay.sharedSecret, _ = aes.NewCipher(sharedSecret[:])
 		client.Relay.sessionId = hex.EncodeToString(response.PublicKey)
+		trace.RecordAction(SharedSecretReq{ClientId: client.ClientConfig.ClientID, NodeType: "Relay"})
 	default:
 		fmt.Println(string(response.PublicKey))
 		return errors.New("Unknown public key type")
@@ -176,6 +239,9 @@ func (client *Client) getRelaySharedSecret() error {
 }
 
 func (client *Client) getExitSharedSecret() error {
+	trace := client.Tracer.CreateTrace()
+	trace.RecordAction(SharedSecretReq{ClientId: client.ClientConfig.ClientID, NodeType: "Exit"})
+
 	priva, puba := role.GetPrivateAndPublicKey()
 	pubaBytes, _ := x509.MarshalPKIXPublicKey(&puba)
 
@@ -199,6 +265,7 @@ func (client *Client) getExitSharedSecret() error {
 		sharedSecret := sha256.Sum256(shared.Bytes())
 		client.Exit.sharedSecret, _ = aes.NewCipher(sharedSecret[:])
 		client.Exit.sessionId = hex.EncodeToString(response.PublicKey)
+		trace.RecordAction(SharedSecretReq{ClientId: client.ClientConfig.ClientID, NodeType: "Exit"})
 	default:
 		fmt.Println(string(response.PublicKey))
 		return errors.New("Unknown public key type")
@@ -210,6 +277,9 @@ func (client *Client) RpcCall(serverAddr string, serviceMethod string, args inte
 	timeouts := 0
 	timeout := 1000
 	done := make(chan bool)
+
+	trace := client.Tracer.CreateTrace()
+	trace.RecordAction(RpcCall{ClientId: client.ClientConfig.ClientID, ServiceMethod: serviceMethod})
 
 	for {
 		exitLayer := role.ReqExitLayer{
@@ -264,6 +334,10 @@ func (client *Client) RpcCall(serverAddr string, serviceMethod string, args inte
 		select {
 		case isDone := <-done:
 			if isDone == true {
+				trace.RecordAction(RpcResRecvd{
+					ClientId:      client.ClientConfig.ClientID,
+					ServiceMethod: serviceMethod,
+					Result:        res})
 				return nil
 			}
 		case <-time.After(time.Duration(timeout) * time.Millisecond):
